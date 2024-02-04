39253fca4052f9924779193e8f6972f0f27e606a
Zach <zachary.j.light@gmail.com>
2024-02-04 14:00:16 -0800

    Beging to add db to backend

---
+++ Backend/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+import utils
+#TODO does this need to be threadsafe for flask?
+class DSGDB:
+    def __init__(self):
+        self.con = sqlite3.connect("test.db")
+        self.cursor = self.con.cursor()
+    def createTables(self):
+        with open("createTables.sql") as f:
+            queries = f.readlines()
+        for query in queries:
+            print(query)
+            self.cursor.execute(query)
+    def newuser(self, username, password, email):
+        hashedPw = utils.getHashedPassword(password)
+        query = f"INSERT INTO users (username,password,email,verified) VALUES ('{username}','{hashedPw.decode('utf-8')}', '{email}', 0);"
+        print(query)
+        self.cursor.execute(query)
+    def getData(self):
+        query = "SELECT * FROM users"
+        print(self.cursor.execute(query).fetchall())
+if __name__ == "__main__":
+    db = DSGDB()
+    # db.createTables()
+    db.newuser("test","testtest", "test@test.com")
+    db.getData()
---
+++ Backend/utils.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+def getHashedPassword(pw):
+    return bcrypt.hashpw(bytes(pw, 'utf-8'), bcrypt.gensalt(15))
+
+def checkPassword(pw, hash):
+    return bcrypt.checkpw(bytes(pw, 'utf-8'), hash)
